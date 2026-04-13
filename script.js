1722ce032bed3627e8815cc5bb21a27202da1c1c
rhuvie <newuser@Rhuvie.local>
2026-04-14 04:06:45 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+hamburger.addEventListener('click', ()=> {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+//Smooth scroll
+
+document.querySelectorAll('a[href^=^"#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behaviour: 'smooth'
+        });
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    });
+});
